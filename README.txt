37164d4bb8fe60a13ed0beaed50f54dfb1527b9a
The Night Phoenix <the-nightphoenix@U-Phoenix.(none)>
2012-04-29 02:44:01 +0200

    first commit

---
+++ README.txt
@@ -0,0 +1,13 @@
+
+Reverse Polish Notation project
+
+
+Simple C++ application to solve arithmetic expressions , with an implementation to shunting-yard algorithm .
+
+Using Standard C++ library and data structures.
+Supporting double values.
+
+It was an assignment project for the Computing Systems Course as part of a larger project to demonstrate the several stages inside a computer to perform calculations and logic.
+
+for more data : https://sites.google.com/site/drahmedakl2/courses2012/computing-system-513/Reverse-Polish-Notation-Project
+
---
+++ main.cpp
@@ -0,0 +1,227 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <cctype>
+
+#include <algorithm>
+#include <stack>
+#include <queue>
+
+using namespace std;
+
+bool is_numircal(char c)
+{
+    return (c >='0' && c <='9') || (c == '.');
+}
+
+bool is_operator(string o)
+{
+    bool result = false;
+    char opr = o[0];
+    string operators = "%*/+-";
+    for (string::size_type i = 0 ; i <operators.length() ; i++ )
+    {
+        result = ( result || (opr == operators[i]) ) ;
+    }
+    return result;
+}
+
+bool is_bracket(string o)
+{
+    return ((o[0] == '(') || (o[0] == ')'));
+}
+
+int opr_order(string o)
+{
+    if (o == "+" || o == "-") return 0;
+    if (o == "*" || o == "/" || o == "%") return 1;
+    if (o == "^") return 2;
+
+}
+
+string validate(string expr)
+{
+    bool valid = false;
+    string valid_str = "";
+
+
+    expr.erase(remove(expr.begin(), expr.end(), ' '), expr.end());
+
+    valid = true;
+    if(!valid) return "";
+
+    for (string::size_type i = 0 ; i < expr.length(); i++)
+    {
+        char c = expr[i];
+        char n = expr[i+1];
+
+        valid_str += c;
+        if ( (is_numircal(n) && !is_numircal(c)) || !is_numircal(n))
+        {
+            valid_str += " ";
+        }
+    }
+    return valid_str;
+}
+
+string get_rpn(string expr) // my implimentation of shunting-yard algorithm
+{
+    string rpn = "";
+    stack<string> op_stack;
+    istringstream iss(expr);
+
+    do
+    {
+        string sub;
+        iss >> sub;
+
+
+        if (sub != "")
+        {
+            if (is_operator(sub))
+            {
+                if(!op_stack.empty())
+                {
+                    // the other empty check for when checking for 2 singl operators in the same preseds
+                    //stack top is null
+                    while((!op_stack.empty()) && (opr_order(sub) <= opr_order(op_stack.top())))
+                    {
+                        rpn += op_stack.top() + " ";
+                        op_stack.pop();
+                    };
+                }
+                op_stack.push(sub);
+            }
+            else if  (is_bracket(sub))
+            {
+                if (sub == ")")
+                {
+                    while(op_stack.top() !="(")
+                    {
+                        rpn += op_stack.top() +" ";
+                        op_stack.pop();
+                    };
+
+                    op_stack.pop();
+                }
+                else
+                {
+                    op_stack.push(sub);
+                }
+            }
+            else
+            {
+                rpn += sub + " ";
+            }
+        }
+    }while(iss);
+
+    while (!op_stack.empty())
+    {
+        rpn += op_stack.top() + " ";
+        op_stack.pop();
+
+    }
+
+    return rpn;
+}
+
+template < class T> // to dump a stack with whtever data type it has
+string get_stack_str(stack<T> s)
+{
+    stack<T> tmp ;
+    ostringstream dump;
+
+
+    while(!s.empty())
+    {
+        dump << s.top() << endl;
+        tmp.push(s.top());
+        s.pop();
+    }
+
+    while (!tmp.empty())
+
+    {
+        s.push(tmp.top());
+        tmp.pop();
+    }
+    return dump.str();
+}
+
+void write_to_file(string data,const char* file_name)
+{
+    ofstream f;
+    f.open(file_name, ios::app);
+    f << data << "\n";
+    f.flush();
+    f.close();
+}
+
+template < class T>
+void dump_stack(stack<T> s)
+{
+    write_to_file(get_stack_str(s),"stack_dump.txt");
+}
+
+double solve(string rpn)
+{
+    stack <double> data;
+    istringstream iss(rpn);
+
+    do {
+        string token;
+        iss >> token;
+
+        if (token == "") continue; // for the empty token.
+
+        // after validation only tokens that are expected are numbers and operators
+        if(!is_operator(token))
+        {
+            double n;
+            istringstream (token)>> n;
+            data.push(n);
+        }
+        else
+        {
+
+            double op2 = data.top();
+            data.pop();
+            double op1 = data.top();
+            data.pop();
+
+            double result;
+
+            if(token == "+") result = op1 + op2;
+            else if (token == "-") result = op1 - op2;
+            else if (token == "*") result = op1 * op2;
+            else if (token == "/") result = op1 / op2;
+            else if (token == "%") result = (int)op1 % (int)op2; // % cant works on ints
+
+            data.push(result);
+        }
+        write_to_file("\n\n === Current Token : " + token + " ===\n","stack_dump.txt");
+        dump_stack(data);
+
+    }while(iss);
+
+    return data.top();
+}
+
+
+int main()
+{
+    //string expr = "( 10.0 + 2 ) + ( 3 + 45 )";
+    //string expr = "5 + 10 - 1";
+//    string expr = "5-1/3/5*(1.5)";
+    string expr = "(7.5*2)/3 + 3 * (5/2)";
+
+
+    cout << "validated : "<< validate(expr) << endl;
+    cout << "RPN : " << get_rpn(validate(expr)) << endl;
+    cout << "result : " << solve(get_rpn(validate(expr))) << endl;
+
+    return 0;
+}
+

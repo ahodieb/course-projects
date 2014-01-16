a07012523de1913da6fc8f37469c6997e5da7240
The Night Phoenix <abdallah.hodieb@gmail.com>
2014-01-16 04:29:24 +0200

    objectized assembler

---
+++ Editor/editor.py
@@ -0,0 +1,74 @@
+__author__ = 'Abdallah Hodieb'
+
+import pygtk,gtk,sys
+
+class ManoAsm(object):
+    def window_destroy(self,widget,data=None):
+        sys.exit(0)
+
+    def window_destroy_event(self,widget,data=None):
+        pass
+
+
+    def btn_open_clicked(self,widget,data=None):
+        dialog = gtk.FileChooserDialog("Open..",
+                               None,
+                               gtk.FILE_CHOOSER_ACTION_OPEN,
+                               (gtk.STOCK_CANCEL, gtk.RESPONSE_CANCEL,
+                                gtk.STOCK_OPEN, gtk.RESPONSE_OK))
+        dialog.set_default_response(gtk.RESPONSE_OK)
+
+        filter = gtk.FileFilter()
+        filter.set_name("All files")
+        filter.add_pattern("*")
+        dialog.add_filter(filter)
+
+        r = dialog.run()
+        response = dialog.run()
+
+        if response == gtk.RESPONSE_OK:
+            f = dialog.get_filename()
+            self.textview1.get_buffer().set_text(open(f).read())
+
+        elif response == gtk.RESPONSE_CANCEL:
+            print 'Closed, no files selected'
+
+        dialog.destroy()
+
+    def btn_asm_clicked(self,widget,data=None):
+
+        b = self.textview1.get_buffer()
+
+        start = b.get_start_iter()
+        end = b.get_end_iter()
+        
+        asm = b.get_text(start,end)
+
+        f = open('/tmp/manoasm.asm','w')
+        f.write(asm)
+        f.close()
+
+    def __init__(self):
+        try:
+            builder = gtk.Builder()
+            builder.add_from_file('gui.glade')
+        except:
+            sys.exit(1)
+
+        self.window = builder.get_object('MainWindow')
+        self.window.set_title('Mano Assembler - By Abdallah Hodieb')
+
+        self.btn_open = builder.get_object('btn_open')
+        self.btn_asm = builder.get_object('btn_asm')
+
+        self.textview1 = builder.get_object('textview1')
+        self.textview2 = builder.get_object('textview2')
+
+
+        builder.connect_signals(self)
+
+
+if __name__ == '__main__':
+    app = ManoAsm()
+    app.window.show()
+    gtk.main()
--- masm.py
+++ masm.py
@@ -13,115 +13,158 @@ ex0 = r'^\s*(\w{0,3}?)\s*\,?\s*(ORG)\s*(\d+)\s*(\/?.*)'
 ex1 = r'^\s*(\w{0,3})\s*\,\s*' + keywords + '\s*([-+]?\w*)\s*(I)?\s*(\/?.*)'
 ex2 = r'^\s*(\w{0,3})\s*\,?\s*' + keywords + '\s*([-+]?\w*)\s*(I)?\s*(\/?.*)'
 
-asm = open(args.asm_files[0]).readlines()
+def main():
 
-symbol_table = {}
-errors = [] #{'msg':'error message','line':3}
+	asm = open(args.asm_files[0]).readlines()
+	a = Assembler(asm)
+	a.generate_symbol_table()
+	a.assemble()
+	print a.str_output()[0]
 
-##pass one
-address_offset = 0
-address_start  = 0
-#getting threshhold
-for l in enumerate(asm):
-	m = re.match(ex0,l[1])
-	if m :
-		#org found 
-		#setting the symbole start adress
-		address_start = l[0]
-		address_offset = int(m.groups()[2])-1
-		
-	m = re.match(ex1,l[1])
-	if m:
-		lable = m.groups()[0]
+	# print 'Symbol','\t','Address'
+	# for k in sorted(a.symbol_table,key=lambda n:a.symbol_table[n]):
+	# 	print k,'\t',a.symbol_table[k]
 
-		if lable == '':
-			errors.append({'msg':'missing lable before , ','line':l[0]+1,'code':'MSLBL','extra':''})
-			continue
+	# print ''
 
-		elif lable in symbol_table.keys():
-			errors.append({'msg':'same lable used bfore','line':l[0]+1,'code':'SMLBL','extra':lable})
-			continue
+	# print 'Address\t','Hex\t','Binary\t','\t','\t','Instruction\t','Operand\t'
+	# for k in sorted(a.program):
+	# 	print k,'\t',	
+	# 	print a.program[k]['hex'].zfill(4).upper(),'\t',	
+	# 	print a.program[k]['bin'].zfill(16),'\t',
+	# 	print a.program[k]['instruction'],'\t'*2,
+	# 	print a.program[k]['operand'],'\t',
+	# 	print ''
 
-		symbol_table[lable] = l[0] + address_offset - address_start
+	# print ''
+
+	# print 'Error','\t'*5,'Line'
+	# for e in a.errors:
+	# 	print e['msg'],'\t'*2,e['line']
 
 
-program = {}
-address_offset = 0
-address_start  = 0
-
-#Pass two
-for l in enumerate(asm):
-	m = re.match(ex0,l[1])
-	if m :
-		
-		address_start = l[0]
-		address_offset = int(m.groups()[2])-1
-		continue
-
-	adrr = l[0] + address_offset - address_start
-	m = re.match(ex2,l[1])
-
-	if m:
-		instruction = m.groups()[1]
-		n = 0
-
-		if instruction in instruction_set.keys():
-
-			n = int(instruction_set[instruction],16)
-
-			if n >= 0 and n < 7 : #memory refrance instruction_set
-				if m.groups()[3] == 'I':
-					n+=7
-
-				n = int(str(n*1000 + symbol_table[m.groups()[2]]),16)
 
 
-			elif n >=int('7001',16) and n <= int('7800',16): #register instruction
-				pass
+class Assembler():
+	def __init__(self,asm):
+
+		self.symbol_table = {}
+		self.program = {}
+		self.errors = [] #{'msg':'error message','line':3}
+		self.program = {}
+		self.asm_file = asm
+
+	def generate_symbol_table(self):
+		##pass one
+		address_offset = 0
+		address_start  = 0
+		#getting threshhold
+		for l in enumerate(self.asm_file):
+			m = re.match(ex0,l[1])
+			if m :
+				#org found 
+				#setting the symbole start adress
+				address_start = l[0]
+				address_offset = int(m.groups()[2])-1
+				
+			m = re.match(ex1,l[1])
+			if m:
+				lable = m.groups()[0]
+
+				if lable == '':
+					self.errors.append({'msg':'missing lable before , ','line':l[0]+1,'code':'MSLBL','extra':''})
+					continue
+
+				elif lable in self.symbol_table.keys():
+					self.errors.append({'msg':'same lable used bfore','line':l[0]+1,'code':'SMLBL','extra':lable})
+					continue
+
+				self.symbol_table[lable] = l[0] + address_offset - address_start
+
+
+	def assemble(self):
+		address_offset = 0
+		address_start  = 0
+
+		#Pass two
+		for l in enumerate(self.asm_file):
+			m = re.match(ex0,l[1])
+			if m :
+				
+				address_start = l[0]
+				address_offset = int(m.groups()[2])-1
+				continue
+
+			adrr = l[0] + address_offset - address_start
+			m = re.match(ex2,l[1])
+
+			if m:
+				instruction = m.groups()[1]
+				n = 0
+
+				if instruction in instruction_set.keys():
+
+					n = int(instruction_set[instruction],16)
+
+					if n >= 0 and n < 7 : #memory refrance instruction_set
+						if m.groups()[3] == 'I':
+							n+=7
+
+						n = int(str(n*1000 + self.symbol_table[m.groups()[2]]),16)
+
+
+					elif n >=int('7001',16) and n <= int('7800',16): #register instruction
+						pass
+					
+					elif n >=int('F040',16) and n <= int('F800',16): #register instruction
+						pass
+					
+					else:
+						pass
+					
+				elif instruction in {'DEC','HEX','END'}:
+					if instruction == 'DEC' : 
+						n = int(m.groups()[2])
+						if n < 0:
+							n = 65536 + n +1
+					elif instruction == 'HEX':
+						n = int(m.groups()[2],16)
+
+					else :
+						# no program should be written after END
+						break
+
+				else:
+					self.errors.append({'msg':'unknown instruction , ','line':l[0]+1,'code':'UKINS','extra':instruction})
+
+				self.program[adrr] = {'instruction':instruction,'operand':m.groups()[2],'bin':bin(n)[2:],'hex':hex(n)[2:]}
 			
-			elif n >=int('F040',16) and n <= int('F800',16): #register instruction
-				pass
-			
+
 			else:
-				pass
-			
-		elif instruction in {'DEC','HEX','END'}:
-			if instruction == 'DEC' : 
-				n = int(m.groups()[2])
-				if n < 0:
-					n = 65536 + n +1
-			elif instruction == 'HEX':
-				n = int(m.groups()[2],16)
+				self.errors.append({'msg':'unknown instruction ','line':l[0]+1,'code':'UKINS','extra':l[1]})
 
-			else :
-				# no program should be written after END
-				break
+	def str_output(self):
+		data =[]
+		data.append(' '.join(['Symbol','\t','Address']))
+		for k in sorted(self.symbol_table,key=lambda n:self.symbol_table[n]):
+			data.append(' '.join([k,'\t',str(self.symbol_table[k]) ] ))
+		data.append('')
 
-		else:
-			errors.append({'msg':'unknown instruction , ','line':l[0]+1,'code':'UKINS','extra':instruction})
+		data.append(' '.join(['Address\t','Hex\t','Binary\t','\t','\t','Instruction\t','Operand\t']))
+		for k in sorted(self.program):
+			data.append(' '.join([str(k),'\t',self.program[k]['hex'].zfill(4).upper(),'\t',self.program[k]['bin'].zfill(16),'\t',self.program[k]['instruction'],'\t'*2,self.program[k]['operand'],'\t']))
 
-		program[adrr] = {'instruction':instruction,'operand':m.groups()[2],'bin':bin(n)[2:],'hex':hex(n)[2:]}
-	
+		data.append('')
 
-	else:
-		errors.append({'msg':'unknown instruction ','line':l[0]+1,'code':'UKINS','extra':l[1]})
+		data.append(' '.join(['Error','\t'*4,'Line']))
+		for e in self.errors:
+			data.append(' '.join([e['msg'],'\t'*2,str(e['line'])]))
 
 
-print 'Symbol','\t','Address'
-for k in sorted(symbol_table,key=lambda n:symbol_table[n]):
-	print k,'\t',symbol_table[k]
 
-print ''
+		return ('\n'.join(data),data)
 
-print 'Address\t','Hex\t','Binary\t','\t','\t','Instruction\t','Operand\t'
-for k in sorted(program):
-	print k,'\t',	
-	print program[k]['hex'].zfill(4).upper(),'\t',	
-	print program[k]['bin'].zfill(16),'\t',
-	print program[k]['instruction'],'\t'*2,
-	print program[k]['operand'],'\t',
-	print ''
+		# print self.errors
 
-print ''
-
-print errors
+if __name__ == '__main__':
+	main()
